9c093092cc3c629540d84f549f05c4501af9e138
trastan <tristan.israel@hotmail.com>
2020-08-31 01:57:13 +0300

    Changed language code.

--- Desktop/djangogirls/mysite/settings.py
+++ Desktop/djangogirls/mysite/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'et'
+LANGUAGE_CODE = 'en-GB'
 
 TIME_ZONE = 'Europe/Tallinn'
 
